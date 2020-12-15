c929328d7ffe04d6706cdc7266b9c96fef87b4b3
anxrsndz <76009814+anxrsndz@users.noreply.github.com>
2020-12-14 18:00:29 -0600

    Create mi-footer.js

---
+++ cmp/mi-footer.js
@@ -0,0 +1,7 @@
+class MiFooter extends HTMLElement {
+connectedCallback(){
+this.innerTHML = /* html */
+`Copyright &copy; 2020 Ana Paola Resendiz Guadarrama`;
+}
+}
+customElement.define(mi-footer", MiFooter);
